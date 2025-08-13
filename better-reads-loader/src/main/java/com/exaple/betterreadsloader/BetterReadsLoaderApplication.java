9c684cbb904d539cea5904e5df20f81d7afcfe31
Smim7 <somayamorshed7@gmail.com>
2025-08-13 16:33:16 -0400

    created better read service

---
+++ better-reads-loader/src/main/java/com/exaple/betterreadsloader/BetterReadsLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.exaple.betterreadsloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterReadsLoaderApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/Book/tracker/app/BookTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Book.tracker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTrackerAppApplication.class, args);
+	}
+
+}
